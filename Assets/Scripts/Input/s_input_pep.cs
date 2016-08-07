79ae785d857696f11f79a3248dd981370d052fe9
Glicknork <glicknork@hotmail.com>
2016-08-07 16:50:17 +0100

    PEP interface code

---
+++ Assets/Scripts/Input/s_input_pep.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class s_input_pep : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("/dev/tty.usbmodem621", 9600);   // open serial Mac = /dev/tty.usbmodem621, PC = COM3 or COM4 ; baud = 9600
+
+    private float f_pressure_init = 0f;
+    private float f_pressure_inter;
+    private int x = 0;
+
+    void Start()
+    {
+        stream.Open();
+        stream.ReadTimeout = 5;                // delay if no data received
+        InvokeRepeating("streamIn", 0.5f, 0.5f);
+    }
+
+    void Update()
+    {
+    }
+
+    void streamIn()
+    {
+        if (stream.IsOpen)
+        {
+            try
+            {
+                string value = stream.ReadLine();    // reads serial port
+                f_pressure_inter = float.Parse(value);
+
+                if (x != 1)                    // routine for each data read - data nr 0 never correct
+                    s_input.f_pressure = f_pressure_inter - f_pressure_init;    // calibrating pressure
+                else                        // saving first valid data for calibration
+                    f_pressure_init = s_input.f_pressure - 10;
+                x++;
+            }
+            catch (System.Exception)            // exit the reading if no value to avoid infinite run
+            {
+            }
+        }
+    }
+}
+
