0a42f4f3cb602d8081a35839ecd5d5f465447c1b
Glicknork <glicknork@hotmail.com>
2016-08-07 23:12:38 +0100

    added lung launch animation

---
+++ Assets/Scripts/LungLaunch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LungLaunch : MonoBehaviour {
+
+    Animator anim;
+    AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Fire");
+            source.Play();
+        }
+	}
+}
