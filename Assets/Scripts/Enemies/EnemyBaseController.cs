fb67d3a426077311b465ef1d00c597b583de09d6
Glicknork <glicknork@hotmail.com>
2016-10-15 13:28:19 +0100

    Begun making enemies move towards player

--- Assets/Scripts/Enemies/EnemyBaseController.cs
+++ Assets/Scripts/Enemies/EnemyBaseController.cs
@@ -12,8 +12,22 @@ public class EnemyBaseController : MonoBehaviour {
     public Rigidbody2D rb;
     [HideInInspector]
     public Rigidbody2D playerRb;
+    [HideInInspector]
+    GameObject lungCharacter;
+
+    Vector2 enemyScreenPos;
+    Vector2 lungCharPos;
+    Vector2 directionOfPlayer;
+    [HideInInspector]
+    public float directionOfTravelX;
+    [HideInInspector]
+    public float directionOfTravelY;
+
+    public RectTransform UITransform;
 
     public GameObject destroyedParticles;
+    
+
 
 	public enum EnemyType
     {
@@ -31,10 +45,19 @@ public class EnemyBaseController : MonoBehaviour {
     public EnemyType enemyType;
 
 	public virtual void Start () {
-
         rb = GetComponent<Rigidbody2D>();
         playerRb = GameController.playerScreen.GetComponent<Rigidbody2D>();
-	}
+
+        // establish the relative direction of the player based on screen position
+        lungCharacter = GameObject.Find("LungCharacter");
+        Transform lungTrans = lungCharacter.transform;
+        lungCharPos = Camera.main.WorldToScreenPoint(lungTrans.position);
+        enemyScreenPos = Camera.main.WorldToScreenPoint(transform.position);        
+        directionOfPlayer = lungCharPos - enemyScreenPos;
+
+        directionOfTravelX = directionOfPlayer.x / Screen.width;
+        directionOfTravelY = directionOfPlayer.y / Screen.height;
+    }
 	
     void OnTriggerEnter2D(Collider2D coll)
     {
