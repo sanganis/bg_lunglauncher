acfcce3f55c65c995f800a270e95b140857556e8
Glicknork <glicknork@hotmail.com>
2016-10-18 13:39:43 +0100

    Tuned background spawning

--- Assets/Scripts/Core/SpawnerController.cs
+++ Assets/Scripts/Core/SpawnerController.cs
@@ -13,6 +13,8 @@ public class SpawnerController : MonoBehaviour
 
     public GameObject[] allBackgroundObjects;
 
+    public GameObject groundTile;
+
     public GameObject backgroundHolder;
 
     public GameObject player;
@@ -22,46 +24,59 @@ public class SpawnerController : MonoBehaviour
     public float startSpawningHeight = 3f;
     public float firstEnemySpawnIncreaseHeight = 100f, secondEnemySpawnIncreaseHeight = 200f, thirdEnemySpawnIncreaseHeight = 300f, fourthEnemySpawnIncreaseHeight = 400f;
     float currentPlayerHeight = 0;
-    float currentSpawnTime = 0;
+    float currentEnemySpawnTime = 0;
+    float currentPowerupSpawnTime = 0;
+
+    // for determing how far the player has travelled, and when to spawn a new ground section
+    public float groundXOffset = 62;
+    float previousGroundXPos;
+    float currentPlayerXPos;
 
     void Start()
-    {
-        // waits for a random ammount of time set between spawnTimeMin and spawnTimeMax before calling an enemy spawn
+    {        
         StartCoroutine("SpawnRandomEnemy");
+        StartCoroutine("SpawnRandomPowerup");
         StartCoroutine("SpawnSkyObjects");
     }
 
     void Update()
     {
         currentPlayerHeight = player.transform.position.y;
+        currentPlayerXPos = player.transform.position.x;
         SetSpawnTimeAccordingToHeight();
+        SpawnGroundSegments();
     }
 
     void SetSpawnTimeAccordingToHeight()
     {
         if (currentPlayerHeight <= startSpawningHeight)
         {
-            currentSpawnTime = 0;
+            currentEnemySpawnTime = 0;
         }
         if (currentPlayerHeight > startSpawningHeight)
         {
-            currentSpawnTime = 2f;
+            currentEnemySpawnTime = 2f;
+            currentPowerupSpawnTime = 5f;
         }
         if (currentPlayerHeight > firstEnemySpawnIncreaseHeight)
         {
-            currentSpawnTime = 1.75f;
+            currentEnemySpawnTime = 1.75f;
+            currentPowerupSpawnTime = 4.75f;
         }
         if (currentPlayerHeight > secondEnemySpawnIncreaseHeight)
         {
-            currentSpawnTime = 1.5f;
+            currentEnemySpawnTime = 1.5f;
+            currentPowerupSpawnTime = 4.5f;
         }
         if (currentPlayerHeight > thirdEnemySpawnIncreaseHeight)
         {
-            currentSpawnTime = 1.25f;
+            currentEnemySpawnTime = 1.25f;
+            currentPowerupSpawnTime = 4.25f;
         }
         if (currentPlayerHeight > fourthEnemySpawnIncreaseHeight)
         {
-            currentSpawnTime = 1f;
+            currentEnemySpawnTime = 1f;
+            currentPowerupSpawnTime = 4f;
         }
     }
 
@@ -69,26 +84,35 @@ public class SpawnerController : MonoBehaviour
     // chooses randomly from one of the spawner transforms and instantiates a new enemy chosen by the method below
     IEnumerator SpawnRandomEnemy()
     {
-        yield return new WaitForSeconds(currentSpawnTime);
-        if (currentSpawnTime > 0)
+        yield return new WaitForSeconds(currentEnemySpawnTime);
+        if (currentEnemySpawnTime > 0)
         {
             Transform newSpawnLocation = allSpawners[Random.Range(0, allSpawners.Length)];
-            Instantiate(ChooseRandomEnemyToSpawn(newSpawnLocation), newSpawnLocation.position, newSpawnLocation.rotation);
-            if (powerupSpawnChance > Random.Range(0f, 1f))
-            {
-                Transform newPowerupSpawnLocation = allSpawners[Random.Range(0, allSpawners.Length)];
-                Instantiate(allPowerups[Random.Range(0, allPowerups.Length)], newSpawnLocation.position, newSpawnLocation.rotation);
-            }
+            Instantiate(ChooseRandomEnemyToSpawn(newSpawnLocation), newSpawnLocation.position, newSpawnLocation.rotation);            
         }
         StartCoroutine("SpawnRandomEnemy");
     }
 
+    IEnumerator SpawnRandomPowerup()
+    {
+        yield return new WaitForSeconds(currentPowerupSpawnTime);
+        if (currentPowerupSpawnTime > 0)
+        {
+            if (powerupSpawnChance > Random.Range(0f, 1f))
+            {
+                Transform newPowerupSpawnLocation = allSpawners[Random.Range(0, allSpawners.Length)];
+                Instantiate(allPowerups[Random.Range(0, allPowerups.Length)], newPowerupSpawnLocation.position, newPowerupSpawnLocation.rotation);
+            }
+        }
+        StartCoroutine("SpawnRandomPowerup");
+    }
 
-    // Randomly chooses from Enemies that should be spawnable at a given spawner location (bottom, side or top)
-    // creates a temporary list and cycles through all the enemies, adding them to the list if they
-    // have the bool checked in EnemyBaseController to be spawnable from that location
-    // then randomly returns an enemy from that list
-    EnemyBaseController ChooseRandomEnemyToSpawn(Transform newSpawnLocation)
+
+        // Randomly chooses from Enemies that should be spawnable at a given spawner location (bottom, side or top)
+        // creates a temporary list and cycles through all the enemies, adding them to the list if they
+        // have the bool checked in EnemyBaseController to be spawnable from that location
+        // then randomly returns an enemy from that list
+        EnemyBaseController ChooseRandomEnemyToSpawn(Transform newSpawnLocation)
     {
         List<EnemyBaseController> enemiesToSpawn = new List<EnemyBaseController>();
         if (newSpawnLocation.gameObject.tag == "BottomSpawner")
@@ -141,5 +165,18 @@ public class SpawnerController : MonoBehaviour
     GameObject ChooseRandomBackgroundToSpawn()
     {        
         return allBackgroundObjects[Random.Range(0, allBackgroundObjects.Length)];
+    }
+
+    void SpawnGroundSegments()
+    {
+        if (currentPlayerXPos > previousGroundXPos + groundXOffset)
+        {
+            Vector3 spawnLoc = new Vector3(currentPlayerXPos + groundXOffset, -8);
+            GameObject newGround;
+            newGround = (GameObject)Instantiate(groundTile, spawnLoc, transform.rotation);
+            newGround.transform.parent = backgroundHolder.transform;
+
+            previousGroundXPos = currentPlayerXPos;
+        }
     }
 }
