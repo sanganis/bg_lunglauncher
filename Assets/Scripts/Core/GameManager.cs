1e9c43366e8803fca1f950d9e5d22885262b2380
Glicknork <glicknork@hotmail.com>
2016-11-12 17:47:12 +0000

    Added new main menu

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _manager = null;
+    public static GameManager manager
+    {
+        get { return _manager; }
+    }
+
+	
+	void Awake ()
+    {
+        if(manager != null && manager != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _manager = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	public void LoadLevel(int levelNumber)
+    {
+        SceneManager.LoadScene(levelNumber, LoadSceneMode.Single);
+    }
+
+
+}
