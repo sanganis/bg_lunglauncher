f7d77354746fad9994e2a8ba84739d09b9c16b53
Glicknork <glicknork@hotmail.com>
2016-11-12 20:28:03 +0000

    Info Message scene added

---
+++ Assets/Scripts/UI/MessageSceneController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MessageSceneController : MonoBehaviour {
+
+    public Text messageText;
+
+    public TextAsset textFile;
+
+    string[] allMessages;
+
+    public float messageTime = 5f;
+
+    	
+	void Start ()
+    {
+        DivideUpMessages();
+        SetRandomMessage();
+        Invoke("StartNextScene", messageTime);
+    }
+
+    void DivideUpMessages()
+    {
+        allMessages = textFile.text.Split("*".ToCharArray());        
+    }
+	
+	
+    void SetRandomMessage()
+    {
+        messageText.text = allMessages[Random.Range(0, allMessages.Length)];
+    }
+
+    void StartNextScene()
+    {
+        GameManager.manager.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
