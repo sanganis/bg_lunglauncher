a016a12b9b51de9eeeef02d051021c1b5b44156d
Glicknork <glicknork@hotmail.com>
2017-02-13 15:41:40 +0000

    Begun work on MyRoom feature

---
+++ Assets/MyRoom/Scripts-MR/Core/MyRoomCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRoomCameraController : MonoBehaviour {
+
+    public static MyRoomCameraController instance;
+
+    [HideInInspector]
+    public Camera camera;
+	
+	void Awake ()
+    {
+        instance = this;
+        camera = GetComponent<Camera>();
+	}
+	
+}
